90f70e5e74ac2dbc5257114c9710e6d692b30e40
Rajendra Chaudhari <rajendra.m.chaudhari2001@gmail.com>
2026-01-16 11:55:13 +0530

    Comment out redundant resume route in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/resume" element={<ResumePage />} />
-          {/* <Route path="/resume/" element={<ResumePage />} /> */}
           <Route path="/projects/:slug" element={<ProjectDetail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
